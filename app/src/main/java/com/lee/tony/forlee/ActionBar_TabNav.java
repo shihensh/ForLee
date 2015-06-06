d3506923956bf48cc0d333437410b9e8db0e41cc
Surf Lee <taolihao@gmail.com>
2015-06-06 16:55:19 +0800

    Initial commit

---
+++ app/src/main/java/com/lee/tony/forlee/ActionBar_TabNav.java
@@ -0,0 +1,25 @@
+package com.lee.tony.forlee;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+
+/**
+ * Created by Tony on 2015/5/24.
+ */
+public class ActionBar_TabNav extends Activity implements ActionBar.TabListener {
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
